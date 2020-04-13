d8434fb077c401e7988210e0e03cdb8296e37bf0
Ayush Mishra <46623370+AyushWebDev@users.noreply.github.com>
2020-04-13 14:11:24 +0530

    Add files via upload

--- router/userRouter.js
+++ router/userRouter.js
@@ -1,5 +1,8 @@
 const express=require('express');
 const User=require('../models/userModel');
+const jwt=require("jsonwebtoken");
+const dotenv=require("dotenv");
+dotenv.config();
 
 const router=express.Router();
 
@@ -52,6 +55,41 @@ router.post('/add',(req,res)=>{
     });
    
 });
+
+router.post("/signin",(req,res)=>{
+    User.findOne({email: req.body.email})
+    .then(u=>{
+        if(!u){
+            return res.json({
+                error: "Account doesn't exist with that email"
+            })
+        }
+
+        if(!u.authenticate(req.body.password)){
+            return res.json({
+                error: "Password is Incorrect.Please Try with correct password again"
+            })
+        }
+
+        const token=jwt.sign({_id: u._id},process.env.JWT_SECRET)
+        res.cookie("signin",token,{expire: new Date()+9999});
+
+        const {_id,username,email}=u;
+        res.json({token,user: {
+            _id,
+            username,
+            email
+        },
+        msg: "Signed In Successfully"
+    })
+
+    })
+});
+
+router.get("/signout",(req,res)=>{
+    res.clearCookie("signin");
+    res.json({msg: "Signed out succesfully"});
+})
 
 router.delete('/:id',(req,res)=>{
     User.findByIdAndDelete(req.params.id)
